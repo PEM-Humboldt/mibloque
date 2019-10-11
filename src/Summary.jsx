5acb872c1ed9d91d9849db35e182f24967bd44de
alegnaaived <alegnaaived@gmail.com>
2019-10-10 19:39:39 -0500

    Implementing MapViewer component with basic configuration and loading new images

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -2,8 +2,18 @@
 import React from 'react';
 
 import CloseIcon from '@material-ui/icons/Close';
+import MapViewer from './commons/MapViewer';
 import Modal from '@material-ui/core/Modal';
 import Layout from './Layout';
+
+// Images to import
+import protegidas from './assets/img/protegidas.png';
+import reservas from './assets/img/reservas.png';
+import estrategicos from './assets/img/estrategicos.png';
+import etnicas from './assets/img/etnicas.png';
+import campesinas from './assets/img/campesinas.png';
+import infraestructura from './assets/img/infraestructura.png';
+import ordenamiento from './assets/img/ordenamiento.png';
 
 class Summary extends React.Component {
   constructor(props) {
@@ -72,6 +82,31 @@ class Summary extends React.Component {
             </button>
             </div>
         </Modal>
+            <section className="sectionintern">
+                <div className="internheader"></div>
+                <div className="map">
+                    <MapViewer /> 
+                </div>
+                <div className="blockdata">
+                    <button className="generalbtn absright">indicadores</button>
+                    <h1>Sobre el bloque</h1>
+                    <div className="line"></div>
+                    <h5 className="hectareas"><b>101,096</b> ha</h5>
+                    <div className="iconos">
+                        <img className="nogo" src={protegidas} alt="Áreas protegidas" title="Áreas protegidas"></img>
+                        <img src={reservas} alt="Reservas forestales" title="Reservas forestales"></img>
+                        <img src={estrategicos} alt="Ecosistemas estratégicos" title="Ecosistemas estratégicos"></img>
+                        <img src={etnicas} alt="Territorios étnicos" title="Territorios étnicos"></img>
+                        <img src={campesinas} alt="Zonas de reserva campesina" title="Zonas de reserva campesina"></img>
+                        <img src={infraestructura} alt="Proyectos e infraestructura" title="Proyectos e infraestructura"></img>
+                        <img src={ordenamiento} alt="Ordenamiento" title="Ordenamiento"></img>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at.<br />
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <h1>Biomas</h1>
+                    <div className="line"></div>
+                </div>
+             </section>
       </Layout>
     );
   }
---
+++ src/commons/MapViewer.jsx
@@ -0,0 +1,103 @@
+/** eslint verified */
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import { Map, TileLayer } from 'react-leaflet';
+
+
+const config = {};
+config.params = {
+  center: [5.2500, -64.9167], // Location: Mariquita-Tolima
+};
+
+class MapViewer extends React.Component {
+  /**
+   * Construct an object with just one value corresponding to a desired attribute
+   *
+   * @param {object} layers layers from props,
+   *  this param is to make the function callable from getDerivedStateFromProps
+   * @param {string} key attribute to choose,
+   *  see attributes of layers inner objects in Search and Compensation.
+   */
+  static infoFromLayers = (layers, key) => {
+    const responseObj = {};
+    Object.keys(layers).forEach((layerKey) => {
+      responseObj[layerKey] = layers[layerKey][key];
+    });
+
+    return responseObj;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      layers: {},
+      activeLayers: [],
+      update: false,
+    };
+
+    this.mapRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    const { layers, activeLayers, update } = this.state;
+    if (update) {
+      Object.keys(layers).forEach((layerName) => {
+        if (activeLayers.includes(layerName)) this.showLayer(layers[layerName], true);
+        else this.showLayer(layers[layerName], false);
+      });
+    }
+    const countActiveLayers = Object.values(activeLayers).filter(Boolean).length;
+    if (countActiveLayers === 0) {
+      this.mapRef.current.leafletElement.setView(config.params.center, 5);
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let newActiveLayers = MapViewer.infoFromLayers(nextProps.layers, 'active');
+    newActiveLayers = Object.keys(newActiveLayers).filter(name => newActiveLayers[name]);
+    const { layers: oldLayers, activeLayers } = prevState;
+    if (newActiveLayers.join() === activeLayers.join()) {
+      return { update: false };
+    }
+
+    const layers = MapViewer.infoFromLayers(nextProps.layers, 'layer');
+    Object.keys(oldLayers).forEach((name) => {
+      if (layers[name] !== oldLayers[name]) {
+        oldLayers[name].remove();
+      }
+    });
+    return { layers, activeLayers: newActiveLayers, update: true };
+  }
+
+  /**
+   *  Defines what layer to show and actions derivate from the selection
+   *
+   * @param {Object} layer receives leaflet object and e.target as the layer
+   * @param {Boolean} state if it's false, then the layer should be hidden
+   */
+  showLayer = (layer, state) => {
+    if (state === false) {
+      this.mapRef.current.leafletElement.removeLayer(layer);
+    } else {
+      this.mapRef.current.leafletElement.addLayer(layer);
+      this.mapRef.current.leafletElement.fitBounds(layer.getBounds());
+    }
+  }
+
+  render() {
+    return (
+      <Map ref={this.mapRef} center={config.params.center} zoom={5} onClick={this.onMapClick}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+      </Map>
+    );
+  }
+}
+
+MapViewer.defaultProps = {
+  layers: []
+};
+
+export default MapViewer;
