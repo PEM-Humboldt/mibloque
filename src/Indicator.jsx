90181ad9dd85148ad5b17ffca516e25797190623
alegnaaived <alegnaaived@gmail.com>
2019-10-25 14:22:13 -0500

    Fixing multi line

--- src/Indicator.jsx
+++ src/Indicator.jsx
@@ -37,24 +37,10 @@ class Indicator extends React.Component {
               <div className="line" />
               <br />
               <p>
-               Lorem ipsum dolor sit amet, consectetuer
-               adipiscing elit, sed diam nonummy nibh euismod
-               tincidunt ut laoreet dolore magna aliquam erat volutpat.
-               Ut wisi enim ad minim veniam, quis nostrud exerci tation
-               ullamcorper suscipit lobortis nisl ut aliquip ex ea
-               commodo consequat. Duis autem vel eum iriure dolor
-               in hendrerit in vulputate velit esse molestie consequat,
-               vel illum dolore eu feugiat nulla facilisis at.
+               Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at.
                 <br />
                 <br />
-                Lorem ipsum dolor sit amet, consectetuer
-               adipiscing elit, sed diam nonummy nibh euismod
-               tincidunt ut laoreet dolore magna aliquam erat volutpat.
-               Ut wisi enim ad minim veniam, quis nostrud exerci tation
-               ullamcorper suscipit lobortis nisl ut aliquip ex ea
-               commodo consequat. Duis autem vel eum iriure dolor
-               in hendrerit in vulputate velit esse molestie consequat,
-               vel illum dolore eu feugiat nulla facilisis at.
+               Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at.
               </p>
               <div className="line" />
             </div>
