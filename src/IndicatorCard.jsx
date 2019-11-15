8f9e7078a75e9296140f080c9acd383dc66568db
Camilo Zapata <cazm013@gmail.com>
2019-11-15 11:48:19 -0500

    * List of biomes on IndicatorDash from mockup on RestAPI, * New declaration of component IndicatorCard as a Class instead of as a Function

--- src/IndicatorCard.jsx
+++ src/IndicatorCard.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const validClassIndicator = (size) => {
-  let validClass = null;
-  switch (size) {
-    case '1':
-      validClass = 'boxes';
-      break;
-    case '2':
-      validClass = 'boxes box2';
-      break;
-    case '3':
-      validClass = 'boxes box3';
-      break;
-    default:
-      validClass = 'boxes';
-      break;
-  }
-  return validClass;
-};
+class IndicatorCard extends React.Component {
+  validClassIndicator = (size) => {
+    let validClass = null;
+    switch (size) {
+      case '1':
+        validClass = 'boxes';
+        break;
+      case '2':
+        validClass = 'boxes box2';
+        break;
+      case '3':
+        validClass = 'boxes box3';
+        break;
+      default:
+        validClass = 'boxes';
+        break;
+    }
+    return validClass;
+  };
 
-const IndicatorCard = ({
-  id, typeName, values, size,
-}) => (
-  <div className={validClassIndicator(size)} key={id}>
-    <h2>
-      {`typeName: ${typeName}`}
-    </h2>
-    <h3>
-      {`size: ${size}`}
-    </h3>
-    <h3>
-      {`values: ${values}`}
-    </h3>
-  </div>
-);
+  render() {
+    const {
+      id, size, typeName, values,
+    } = this.props;
+    return (
+      <div className={this.validClassIndicator(size)} key={id}>
+        <h2>
+          {`typeName: ${typeName}`}
+        </h2>
+        <h3>
+          {`size: ${size}`}
+        </h3>
+        <h3>
+          {`values: ${values}`}
+        </h3>
+      </div>
+    );
+  }
+}
 
 IndicatorCard.propTypes = {
   id: PropTypes.string,
