c87d32d86cd64113e1f0984b21a18ee7b9e16fb4
Erika Suárez Valencia <esuarez@humboldt.org.co>
2019-12-06 12:23:11 -0500

    Update App, Home and Summary components to pass area name as URL parameter in Summary.
    - Also move API requests for sedimentary list and area info to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Home';
 import Summary from './Summary';
 import IndicatorsDash from './IndicatorsDash';
 import Indicator from './Indicator';
+import RestAPI from './commons/RestAPI';
 import './assets/main.css';
 
 class App extends React.Component {
@@ -13,14 +14,47 @@ class App extends React.Component {
     super(props);
     this.state = {
       activeArea: null,
+      sedimentaryList: [],
     };
   }
 
-  setActiveArea = (value) => {
-    this.setState({ activeArea: value });
+  async componentDidMount() {
+    try {
+      const sedimentary = await RestAPI.requestSedimentaryBasins();
+      this.setState({
+        sedimentaryList: sedimentary,
+      });
+    } catch (error) {
+      // TODO: Set state in a error (handling error)
+    }
   }
 
-  loadHome = () => (<Home setActiveArea={this.setActiveArea} />)
+  setActiveArea = async (name) => {
+    const { sedimentaryList } = this.state;
+    try {
+      const area = await RestAPI.requestAreaSelected(name);
+      this.setState({
+        activeArea: {
+          sedimentary_name: sedimentaryList.find((item) => (
+            item.code === area.sedimentary_code
+          )).name,
+          ...area,
+        },
+      });
+    } catch (error) {
+      // TODO: What to do with the error?
+    }
+  }
+
+  loadHome = () => {
+    const { sedimentaryList } = this.state;
+    return (
+      <Home
+        setActiveArea={this.setActiveArea}
+        sedimentaryList={sedimentaryList}
+      />
+    );
+  }
 
   loadIndicatorsDash = () => {
     const { activeArea } = this.state;
@@ -40,11 +74,13 @@ class App extends React.Component {
     );
   }
 
-  loadSummary = () => {
+  loadSummary = ({ match }) => {
     const { activeArea } = this.state;
     return (
       <Summary
         activeArea={activeArea}
+        areaName={match.params.name}
+        setActiveArea={this.setActiveArea}
       />
     );
   }
@@ -56,7 +92,7 @@ class App extends React.Component {
           <Route exact path="/" render={this.loadHome} />
           <Route exact path="/indicatorsDash" render={this.loadIndicatorsDash} />
           <Route exact path="/indicator" render={this.loadIndicator} />
-          <Route exact path="/summary" render={this.loadSummary} />
+          <Route path="/summary/:name?" render={this.loadSummary} />
         </Switch>
       </main>
     );
--- src/Summary.jsx
+++ src/Summary.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import L from 'leaflet';
 import CloseIcon from '@material-ui/icons/Close';
 import Modal from '@material-ui/core/Modal';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import MapViewer from './commons/MapViewer';
 import Layout from './Layout';
 import RenderGraph from './graphs/RenderGraph';
@@ -44,16 +44,35 @@ class Summary extends React.Component {
       ],
       connError: false,
       layers: {},
+      redirect: false,
     };
   }
 
-  async componentDidMount() {
-    const { activeArea } = this.props;
+  componentDidMount() {
+    const { areaName } = this.props;
+    if (!areaName) {
+      this.setState({ redirect: true });
+    } else {
+      this.loadData();
+    }
+  }
+
+  componentDidUpdate() {
+    const { activeArea, areaName, setActiveArea } = this.props;
+    if (!activeArea) {
+      setActiveArea(areaName);
+    }
+  }
+
+  /**
+   * Load data required by the component
+   */
+  loadData = async () => {
     const { colors } = this.state;
-    const validData = activeArea && activeArea.name;
-    if (validData) {
-      const geometryRequest = await RestAPI.requestBiomesGeometryWithArea(activeArea.name);
-      const biomesRequest = await RestAPI.requestBiomesDataByArea(activeArea.name);
+    const { areaName } = this.props;
+    try {
+      const geometryRequest = await RestAPI.requestBiomesGeometryWithArea(areaName);
+      const biomesRequest = await RestAPI.requestBiomesDataByArea(areaName);
       const dictionaryColor = {};
       let biomesCounter = 0;
       biomesRequest.forEach((biome) => {
@@ -65,7 +84,7 @@ class Summary extends React.Component {
         colorPerBiome: dictionaryColor,
         layers: {
           area: {
-            displayName: activeArea.name,
+            displayName: areaName,
             id: 1,
             active: true,
             layer: L.geoJSON(geometryRequest, {
@@ -78,10 +97,9 @@ class Summary extends React.Component {
           },
         },
       });
-    } else {
-      this.setState({
-        connError: true,
-      });
+    } catch (error) {
+      // TODO: Decide if to do something else with the error
+      this.reportConnError();
     }
   }
 
@@ -115,185 +133,190 @@ class Summary extends React.Component {
 
   render() {
     const {
-      biomesDataGraps, connError, layers,
+      biomesDataGraps, connError, layers, redirect,
     } = this.state;
     const { activeArea } = this.props;
+    if (redirect) {
+      return (<Redirect to="/" />);
+    }
+    if (!activeArea) return null;
     return (
-      (activeArea && (
-        <Layout
-          activateHome
-          activeArea={activeArea}
+      <Layout
+        activateHome
+        activeArea={activeArea}
+      >
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={connError}
+          onClose={this.handleCloseModal('connError')}
+          disableAutoFocus
         >
-          <Modal
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-            open={connError}
-            onClose={this.handleCloseModal('connError')}
-            disableAutoFocus
-          >
-            <div className="generalAlarm">
-              <h2>
-                <b>Sin conexión al servidor</b>
-                <br />
-                  Intenta de nuevo en unos minutos.
-              </h2>
+          <div className="generalAlarm">
+            <h2>
+              <b>Sin conexión al servidor</b>
+              <br />
+                Intenta de nuevo en unos minutos.
+            </h2>
+            <button
+              type="button"
+              className="closebtn"
+              onClick={this.handleCloseModal('connError')}
+              data-tooltip
+              title="Cerrar"
+            >
+              <CloseIcon />
+            </button>
+          </div>
+        </Modal>
+        <section className="sectionintern">
+          <div className="internheader" />
+          <div className="map">
+            <MapViewer
+              layers={layers}
+            />
+          </div>
+          <div className="blockdata">
+            <Link to="/indicatorsDash">
               <button
                 type="button"
-                className="closebtn"
-                onClick={this.handleCloseModal('connError')}
-                data-tooltip
-                title="Cerrar"
+                key="indBtn"
+                className="generalbtn absright"
               >
-                <CloseIcon />
+                indicadores
               </button>
-            </div>
-          </Modal>
-          <section className="sectionintern">
-            <div className="internheader" />
-            <div className="map">
-              <MapViewer
-                layers={layers}
+            </Link>
+            <h1>Sobre el área</h1>
+            <div className="line" />
+            <h5 className="hectareas vulne">
+              Vulnerabilidad:
+              {' '}
+              <b>{activeArea.vulnerability ? numberWithCommas(Number(activeArea.vulnerability).toFixed(2)) : 'Sin información disponible'}</b>
+            </h5>
+            <h5 className="hectareas">
+              <b>{activeArea.area ? numberWithCommas(Number(activeArea.area).toFixed(2)) : 'Sin información disponible'}</b>
+              {' '}
+              ha
+            </h5>
+            <div className="iconos">
+              <img
+                className={activeArea.categories.protected_areas ? '' : 'nogo'}
+                src={protegidas}
+                alt="Áreas protegidas"
+                title="Áreas protegidas"
+              />
+              <img
+                className={activeArea.categories.forest_reserves ? '' : 'nogo'}
+                src={reservas}
+                alt="Reservas forestales"
+                title="Reservas forestales"
+              />
+              <img
+                className={activeArea.categories.strategic_ecosystems ? '' : 'nogo'}
+                src={estrategicos}
+                alt="Ecosistemas estratégicos"
+                title="Ecosistemas estratégicos"
+              />
+              <img
+                className={activeArea.categories.ethnic_territories ? '' : 'nogo'}
+                src={etnicas}
+                alt="Territorios étnicos"
+                title="Territorios étnicos"
+              />
+              <img
+                className={activeArea.categories.peasant_reserves ? '' : 'nogo'}
+                src={campesinas}
+                alt="Zonas de reserva campesina"
+                title="Zonas de reserva campesina"
+              />
+              <img
+                className={activeArea.categories.projects ? '' : 'nogo'}
+                src={infraestructura}
+                alt="Proyectos e infraestructura"
+                title="Proyectos e infraestructura"
+              />
+              <img
+                className={activeArea.categories.ordering ? '' : 'nogo'}
+                src={ordenamiento}
+                alt="Ordenamiento"
+                title="Ordenamiento"
+              />
+              <img
+                className={activeArea.categories.international ? '' : 'nogo'}
+                src={internacional}
+                alt="Protección internacional"
+                title="Protección internacional"
+              />
+              <img
+                className={activeArea.categories.license ? '' : 'nogo'}
+                src={licencias}
+                alt="Licencias ambientales"
+                title="Licencias ambientales"
               />
             </div>
-            <div className="blockdata">
-              <Link to="/indicatorsDash">
-                <button
-                  type="button"
-                  key="indBtn"
-                  className="generalbtn absright"
-                >
-                  indicadores
-                </button>
-              </Link>
-              <h1>Sobre el área</h1>
-              <div className="line" />
-              <h5 className="hectareas vulne">
-                Vulnerabilidad:
-                {' '}
-                <b>{activeArea.vulnerability ? numberWithCommas(Number(activeArea.vulnerability).toFixed(2)) : 'Sin información disponible'}</b>
-              </h5>
-              <h5 className="hectareas">
-                <b>{activeArea.area ? numberWithCommas(Number(activeArea.area).toFixed(2)) : 'Sin información disponible'}</b>
-                {' '}
-                ha
-              </h5>
-              <div className="iconos">
-                <img
-                  className={activeArea.categories.protected_areas ? '' : 'nogo'}
-                  src={protegidas}
-                  alt="Áreas protegidas"
-                  title="Áreas protegidas"
-                />
-                <img
-                  className={activeArea.categories.forest_reserves ? '' : 'nogo'}
-                  src={reservas}
-                  alt="Reservas forestales"
-                  title="Reservas forestales"
-                />
-                <img
-                  className={activeArea.categories.strategic_ecosystems ? '' : 'nogo'}
-                  src={estrategicos}
-                  alt="Ecosistemas estratégicos"
-                  title="Ecosistemas estratégicos"
-                />
-                <img
-                  className={activeArea.categories.ethnic_territories ? '' : 'nogo'}
-                  src={etnicas}
-                  alt="Territorios étnicos"
-                  title="Territorios étnicos"
-                />
-                <img
-                  className={activeArea.categories.peasant_reserves ? '' : 'nogo'}
-                  src={campesinas}
-                  alt="Zonas de reserva campesina"
-                  title="Zonas de reserva campesina"
-                />
-                <img
-                  className={activeArea.categories.projects ? '' : 'nogo'}
-                  src={infraestructura}
-                  alt="Proyectos e infraestructura"
-                  title="Proyectos e infraestructura"
-                />
-                <img
-                  className={activeArea.categories.ordering ? '' : 'nogo'}
-                  src={ordenamiento}
-                  alt="Ordenamiento"
-                  title="Ordenamiento"
-                />
-                <img
-                  className={activeArea.categories.international ? '' : 'nogo'}
-                  src={internacional}
-                  alt="Protección internacional"
-                  title="Protección internacional"
-                />
-                <img
-                  className={activeArea.categories.license ? '' : 'nogo'}
-                  src={licencias}
-                  alt="Licencias ambientales"
-                  title="Licencias ambientales"
-                />
-              </div>
-              <p>
-                La información espacial que se muestra en la figura corresponde a la identificación de condiciones sociales, culturales, económicas y biofísicas, algunas de las cuales son determinantes ambientales del ordenamiento territorial y pueden generar restricciones al desarrollo de las actividades de exploración y explotación de hidrocarburos. Este análisis hace parte del procedimiento que la ANH ha establecido para la coordinación y concurrencia con las entidades territoriales y demás autoridades y entidades con presencia en el territorio, con el fin de posibilitar la definición y determinación de nuevas áreas de interés de hidrocarburos.
-              </p>
-              <h1>Biomas</h1>
-              <div className="line" />
-              <br />
-              <div>
-                {
-                  biomesDataGraps && Object.values(biomesDataGraps).map((biome) => {
-                    const localColor = this.getColorCode(biome.name);
-                    return (
-                      <div key={biome.name}>
-                        {
-                          RenderGraph(
-                            [
-                              biome,
-                              {
-                                area: (activeArea.area - biome.area),
-                                type: 'empty',
-                                color: '#fff',
-                              },
-                            ], '', '', 'SmallBarStackGraph',
-                            biome.name, '', [localColor, '#fff'], 'ha',
-                          )
-                        }
-                        {biome.area ? (
-                          <span key={biome.area}>
-                            <b>
-                              {`${numberWithCommas(Number(biome.area).toFixed(2))} `}
-                            </b>
-                              ha
-                          </span>
-                        ) : ''}
-                        {biome.compensation_factor ? (
-                          <span key={biome.compensation_factor}>
-                             · FC:
-                            <b>
-                              {`${numberWithCommas(Number(biome.compensation_factor).toFixed(2))} `}
-                            </b>
-                          </span>
-                        ) : ''}
-                      </div>
-                    );
-                  })
-                }
-              </div>
+            <p>
+              La información espacial que se muestra en la figura corresponde a la identificación de condiciones sociales, culturales, económicas y biofísicas, algunas de las cuales son determinantes ambientales del ordenamiento territorial y pueden generar restricciones al desarrollo de las actividades de exploración y explotación de hidrocarburos. Este análisis hace parte del procedimiento que la ANH ha establecido para la coordinación y concurrencia con las entidades territoriales y demás autoridades y entidades con presencia en el territorio, con el fin de posibilitar la definición y determinación de nuevas áreas de interés de hidrocarburos.
+            </p>
+            <h1>Biomas</h1>
+            <div className="line" />
+            <br />
+            <div>
+              {
+                biomesDataGraps && Object.values(biomesDataGraps).map((biome) => {
+                  const localColor = this.getColorCode(biome.name);
+                  return (
+                    <div key={biome.name}>
+                      {
+                        RenderGraph(
+                          [
+                            biome,
+                            {
+                              area: (activeArea.area - biome.area),
+                              type: 'empty',
+                              color: '#fff',
+                            },
+                          ], '', '', 'SmallBarStackGraph',
+                          biome.name, '', [localColor, '#fff'], 'ha',
+                        )
+                      }
+                      {biome.area ? (
+                        <span key={biome.area}>
+                          <b>
+                            {`${numberWithCommas(Number(biome.area).toFixed(2))} `}
+                          </b>
+                            ha
+                        </span>
+                      ) : ''}
+                      {biome.compensation_factor ? (
+                        <span key={biome.compensation_factor}>
+                            · FC:
+                          <b>
+                            {`${numberWithCommas(Number(biome.compensation_factor).toFixed(2))} `}
+                          </b>
+                        </span>
+                      ) : ''}
+                    </div>
+                  );
+                })
+              }
             </div>
-          </section>
-        </Layout>
-      ))
-      // TODO: Redirect to HOME if activeArea is empty
+          </div>
+        </section>
+      </Layout>
     );
   }
 }
 
 Summary.propTypes = {
   activeArea: PropTypes.object,
+  areaName: PropTypes.string,
+  setActiveArea: PropTypes.func,
 };
 
 Summary.defaultProps = {
   activeArea: null,
+  areaName: null,
+  setActiveArea: () => {},
 };
 
 export default Summary;
