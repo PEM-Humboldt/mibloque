c371a71c306f0878fef927d06c9ccfe472fdb87b
Camilo Zapata <cazm013@gmail.com>
2019-12-27 16:28:34 -0500

    Label for button INDICADORES IS changued for INFO according to Cesar suggestion

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -206,7 +206,7 @@ class Summary extends React.Component {
                 key="indBtn"
                 className={`generalbtn absright ${nogo ? 'nogo' : ''}`}
               >
-                indicadores
+                info
               </button>
             </Link>
             <h1>Sobre el área</h1>
